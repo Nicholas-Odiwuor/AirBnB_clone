a140e566b07c0df9e2b0e10bbfeb00f5f8c8efa5
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-03-25 00:30:33 -0700

    Airbnb clone

---
+++ base_model.py
@@ -0,0 +1,10 @@
+class BaseModel:
+    def __init__(self, name):
+        self.name = name
+
+    def display_info(self):
+        print("Name:", self.name)
+
+def base_function():
+    print("This is a base function.")
+
